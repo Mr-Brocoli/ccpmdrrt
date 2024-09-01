b88298b8a3940cac11b1119774846beeca7d18b2
MrBrocoli <59612417+Mr-Brocoli@users.noreply.github.com>
2024-09-01 02:13:31 -0600

    A semi working spawn enemies at runtime effect

--- pmdrrt.cs
+++ pmdrrt.cs
@@ -28,6 +28,9 @@ namespace CrowdControl.Games.Packs
             public uint SPEEDUP_ADDR = 0x108;
             public uint SLOWDOWN_ADDR = 0x10D;
 
+            public uint XPOS = 0x184;
+            public uint YPOS = 0x186;
+
             public Pokemon(uint baseAddress)
             {
                 baseAddr = baseAddress;
@@ -35,6 +38,8 @@ namespace CrowdControl.Games.Packs
                 MAXHP_ADDR += baseAddr;
                 SPEEDUP_ADDR += baseAddr;
                 SLOWDOWN_ADDR += baseAddr;
+                XPOS += baseAddr;
+                YPOS += baseAddr;
             }
 
 
@@ -54,7 +59,55 @@ namespace CrowdControl.Games.Packs
         }
 
 
-        private Pokemon one = new Pokemon(0x02004190);
+        /*struct unkStruct_806B7F8
+            {
+                u16 species;
+                u8 unk2;
+                u32 unk4;
+                u16 level;
+                u8 fillA[0xC - 0xA];
+                struct Position pos;
+                u8 unk10;
+            };*/
+
+        private bool spawnPokemon(ushort pID, ushort x, ushort y)
+        {
+
+            Connector.Read16(one.XPOS, out x);
+            Connector.Read16(one.YPOS, out y);
+            y -= 1;
+
+            uint injectsite = 0x02003af4 + 0xc0;
+
+            //if (!Connector.Read16(injectsite + 0xa, out ushort latch) || latch != 0)
+            //    return false;
+
+            bool check = Connector.Write16(injectsite, pID) && Connector.Write16(injectsite + 2, 0)
+                && Connector.Write32(injectsite + 4, 0) && Connector.Write16(injectsite + 8, 1)
+                && Connector.Write16(injectsite + 0xa, 1) && Connector.Write16(injectsite + 0xc, x)
+                && Connector.Write16(injectsite + 0xe, y) && Connector.Write8(injectsite + 0x11, 0);
+
+            if (!check)
+                return false;
+
+            byte[] mold = new byte[] { 0xc0, 0x22, 0x80, 0x18, 0x0a, 0x23, 0xc4, 0x5e, 0x00, 0x22, 0xc2, 0x52, 0xc0, 0x46, 0xc0, 0x46, 0x00, 0x21, 0xc0, 0x46, 0x00, 0x2c, 0x68, 0xd0, 0x26, 0xf0, 0x1e, 0xff };
+            for (uint i = 0; i != mold.Length; i++)
+            {
+                uint j = (i / 4) * 4;
+                uint k = i % 4;
+                if (!Connector.Write8(0x080449a0 + j + k, mold[i]))
+                    return false;
+            }
+
+            //Connector.Write16(0x080449a0, 0xc046);
+
+            //byte[] old = new byte[] { 0xcc, 0x22, 0xd2, 0x00, 0x80, 0x18, 0x00, 0x23, 0xc0, 0x5e, 0x40, 0x00, 0x32, 0x31, 0x40, 0x18, 0x00, 0x21, 0x40, 0x5e, 0x00, 0x28, 0x68, 0xd0, 0x2d, 0xf0, 0xc6, 0xf8 };
+
+            return true;
+        }
+
+
+    private Pokemon one = new Pokemon(0x02004190);
 
 
         public override EffectList Effects
@@ -66,6 +119,7 @@ namespace CrowdControl.Games.Packs
                     new Effect("OHKO", "ohko"){Price=10, Duration=10},
                     new Effect("Speedup (50 movements)", "speedup_50"){Price=10},
                     new Effect("Slowdown (50 movements)", "slowdown_50"){Price=10},
+                    new Effect("Spawn test", "spawntest"){Price=10},
                 };
                 return effects;
             }
@@ -92,6 +146,9 @@ namespace CrowdControl.Games.Packs
                     return;
                 case "slowdown":
                     TryEffect(request, () => canSpeedOrSlow(one), () => Connector.Write8(one.SLOWDOWN_ADDR, byte.Parse(codeParams[1])));
+                    return;
+                case "spawntest":
+                    TryEffect(request, () => true, () => spawnPokemon(0x17c, 6, 6));
                     return;
             }
         }
